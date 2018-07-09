15553f6e766c35561e19d2fa609aab4cc5fd6da9
vicky-sunshine <lch82327@gmail.com>
2018-07-09 11:28:59 +0800

    change model object name

--- run_this.py
+++ run_this.py
@@ -1,10 +1,11 @@
 from maze_env import Maze
-from RL_brain import QLearningTable
+from dqn import DeepQNetwork
 
 
-def update():
-    for episode in range(1000):
-        print("episode: " + str(episode))
+def run_maze():
+    step = 0
+    episode = 0
+    while True:
         # initial observation
         observation = env._reset()
 
@@ -12,14 +13,16 @@ def update():
             # fresh env
             env._render()
 
-            # RL choose action based on observation
-            action = RL.choose_action(str(observation))
+            # model choose action based on observation
+            action = model.choose_action(step, observation)
 
-            # RL take action and get next observation and reward
+            # model take action and get next observation and reward
             observation_, reward, done = env._step(action)
 
-            # RL learn from this transition
-            RL.learn(str(observation), action, reward, str(observation_))
+            model.store_transition(observation, action, reward, observation_)
+
+            if (step > 200) and (step % 5 == 0):
+                model.learn()
 
             # swap observation
             observation = observation_
@@ -27,13 +30,29 @@ def update():
             # break while loop when end of this episode
             if done:
                 break
+            step += 1
+
+        episode += 1
+        print("episode: "+str(episode))
 
     # end of game
     print('game over')
-    # env.destroy()
+    env.destroy()
 
 
 if __name__ == "__main__":
+    # maze game
     env = Maze()
-    RL = QLearningTable(actions=list(range(env.n_actions)))
-    update()
+    model = DeepQNetwork(env.n_actions, env.n_features,
+                         learning_rate=0.01,
+                         reward_decay=0.9,
+                         replace_trainee_iter=2000,
+                         save_trainee_iter=2000,
+                         memory_size=2000,
+                         eps_min=0.1,
+                         eps_max=1.0,    # the higher, more random
+                         eps_decay_steps=2000000,
+                         checkpoint_path="checkpoint/maze.ckpt",
+                         # output_graph=True
+                         )
+    run_maze()
