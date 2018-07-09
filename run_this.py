49bc8e59644772661a12e2cf5e3286cd9c4e97be
vicky-sunshine <lch82327@gmail.com>
2018-07-09 13:43:37 +0800

    change hyperparameter

--- run_this.py
+++ run_this.py
@@ -5,7 +5,7 @@ from dqn import DeepQNetwork
 def run_maze():
     step = 0
     episode = 0
-    while True:
+    for episode in range(5000):
         # initial observation
         observation = env._reset()
 
@@ -21,7 +21,7 @@ def run_maze():
 
             model.store_transition(observation, action, reward, observation_)
 
-            if (step > 200) and (step % 5 == 0):
+            if (step > 1000) and (step % 5 == 0):
                 model.learn()
 
             # swap observation
@@ -32,27 +32,28 @@ def run_maze():
                 break
             step += 1
 
-        episode += 1
+        # episode += 1
         print("episode: "+str(episode))
 
     # end of game
     print('game over')
-    env.destroy()
 
 
 if __name__ == "__main__":
     # maze game
     env = Maze()
     model = DeepQNetwork(env.n_actions, env.n_features,
+                         batch_size=64,
                          learning_rate=0.01,
                          reward_decay=0.9,
-                         replace_trainee_iter=2000,
+                         replace_trainee_iter=5000,
                          save_trainee_iter=2000,
-                         memory_size=2000,
+                         memory_size=10000,
                          eps_min=0.1,
-                         eps_max=1.0,    # the higher, more random
+                         eps_max=0.6,    # the higher, more random
                          eps_decay_steps=2000000,
                          checkpoint_path="checkpoint/maze.ckpt",
                          # output_graph=True
                          )
     run_maze()
+    # model.plot_cost()
